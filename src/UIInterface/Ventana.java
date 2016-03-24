e2fa01db0bc9efd4acab4805630a993c16c5819c
eshernandez2 <erika.programmer@gmail.com>
2016-03-23 19:50:15 -0430

    test

--- src/UIInterface/Ventana.java
+++ src/UIInterface/Ventana.java
@@ -1,9 +1,16 @@
 package UIInterface;
 
 import UIHelper.TreeWindow;
+import andortree.AndOrTree;
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -19,10 +26,13 @@ import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 
 public class UIInterface extends JFrame {
